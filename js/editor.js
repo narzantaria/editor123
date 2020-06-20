037423a7119047cf8198b970d7196b3d9c6b84e0
Nar Zantaria <narzantaria@gmail.com>
2020-06-20 20:43:13 +0300

    Load and Selection

--- js/editor.js
+++ js/editor.js
@@ -1,40 +1,32 @@
-// get current text of the input
-var content = input.innerHTML, contentProxy;
+// window.addEventListener("load", function () {
+//   document.getElementById('editor').addEventListener('paste', function (e) {
+//     e.preventDefault();
+//     e.stopPropagation();
+//     insertAtCursor(this, e.clipboardData.getData('text/html'));
+//   });
+// });
 
-window.addEventListener("load", function () {
-  document.getElementById('editor').addEventListener('paste', function (e) {
-    e.preventDefault();
-    e.stopPropagation();
-    insertAtCursor(this, e.clipboardData.getData('text/html'));
-  });
-});
-
-document.getElementById('showtags').addEventListener('click', function () {
+document.getElementById('code').addEventListener('click', function () {
   var editor = document.getElementById('editor');
   var content = editor.innerHTML;
   content = htmlEncode(content);
   editor.innerHTML = content;
 });
 
-function insertAtCursor(input, textToInsert) {
-  // save selection start and end position
-  const start = window.getSelection().anchorOffset;
-  const end = window.getSelection().focusOffset;
+// function insertAtCursor(input, textToInsert) {
+//   // get current text of the input
+//   var content = input.innerHTML;
 
-  // console.log(start);
-  // console.log(end);
-  // console.log(content.slice(0, start));
-  // console.log(content.slice(end));
-  // console.log(textToInsert);
-  // console.log(content.slice(0, start) + textToInsert + content.slice(end));
+//   // save selection start and end position
+//   const start = window.getSelection().anchorOffset;
+//   const end = window.getSelection().focusOffset;
 
-  // update the content with our text inserted
-  contentProxy = content.slice(0, start) + textToInsert + content.slice(end);
-  input.innerHTML = contentProxy;
+//   // update the content with our text inserted
+//   input.innerHTML = content.slice(0, start) + textToInsert + content.slice(end);
 
-  // update cursor to be at the end of insertion
-  input.selectionStart = input.selectionEnd = start + textToInsert.length;
-}
+//   // update cursor to be at the end of insertion
+//   input.selectionStart = input.selectionEnd = start + textToInsert.length;
+// }
 
 function htmlEncode(html) {
   html = trim(html);
@@ -57,3 +49,23 @@ function htmlEncode(html) {
 function trim(input) {
   return input.toString().replace(/^([\s]*)|([\s]*)$/g, '');
 }
+
+document.getElementById('bold').addEventListener('click', function () {
+  let span = document.createElement("span");
+  span.style.fontWeight = "bold";
+  if (window.getSelection) {
+    let sel = window.getSelection();
+    console.log(sel.anchorNode.parentNode);
+    console.log(sel.anchorOffset);
+    if (sel.rangeCount) {
+      let range = sel.getRangeAt(0).cloneRange();
+      range.surroundContents(span);
+      sel.removeAllRanges();
+      sel.addRange(range);
+    }
+  }
+});
+
+document.getElementById('italic').addEventListener('click', function () {
+  //
+});
